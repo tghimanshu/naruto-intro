0af132d2a851430ee3c75c587f0fcb7f529da451
Himanshu Gohil <himanshugohil234@gmail.com>
2026-02-20 11:11:41 +0000

    #12: Confirmed Multiple character portabilitiy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   /**
@@ -19,4 +19,26 @@ export class AppComponent {
    * This property is bound to the template and displays the application's name.
    */
   title = 'naruto-intro';
+  character = {
+    firstName: 'SASUKE',
+    lastName: 'UCHIHA',
+    splits: ['SA', 'SU', 'KE'],
+    firstNameKanji: 'サスケ',
+    lastNameKanji: 'うちは',
+    introduction:
+      'Sasuke Uchiha is one of the main characters in the Naruto series. He is a member of the Uchiha clan, a powerful and prestigious family known for their Sharingan abilities. Sasuke is initially portrayed as a rival to Naruto Uzumaki, but his character undergoes significant development throughout the series.',
+    imageUri: 'assets/sasuke-uchiha.png',
+    primaryColor: '#1e336a',
+  };
+  // character = {
+  //   firstName: 'NARUTO',
+  //   lastName: 'UZUMAKI',
+  //   splits: ['NA', 'RU', 'TO'],
+  //   firstNameKanji: 'ナルト',
+  //   lastNameKanji: 'うずまき',
+  //   introduction:
+  //     ' Naruto Uzumaki is a titular protagonist of the series . He waws the first character created by Kishimoto during the conception of the series and was designed with many traits from other shonen characters.',
+  //   imageUri: 'assets/naruto-sage-mode.png',
+  //   primaryColor: '#8b3125',
+  // };
 }
